49e7449fcb7ef3f1f1e2872ce8a65e26047970fb
Krishnasai Addala <75469758+D0nu-t@users.noreply.github.com>
2026-06-26 22:16:55 +0530

    kmskmskms

--- src/graph.ts
+++ src/graph.ts
@@ -385,6 +385,9 @@ const RAW_LINKS: { source: string; target: string }[] = [
 { source: 'capgemini', target: 'langchain' },
 { source: 'capgemini', target: 'openai' },
 { source: 'capgemini', target: 'docker' },
+{ source: 'capgemini', target: 'ETL' },
+{ source: 'capgemini', target: 'AWS' },
+{ source: 'capgemini', target: 'BigQuery' },
 
 
 ]
